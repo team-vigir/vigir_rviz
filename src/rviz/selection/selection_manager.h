fdd9bd9022dea1777fe5549a44fb0d705c24f9e0
vigir@gandalf <vigir@gandalf>
2013-11-28 16:16:07 -0500

    - fixing selection

--- src/rviz/selection/selection_manager.h
+++ src/rviz/selection/selection_manager.h
@@ -79,6 +79,7 @@ class SelectionManager: public QObject, public Ogre::MaterialManager::Listener, 
 {
 Q_OBJECT
 public:
+
   enum SelectType
   {
     Add,
@@ -199,6 +200,8 @@ public:
 
   PropertyTreeModel* getPropertyModel() { return property_model_; }
 
+  void setOrthoConfig( Ogre::Viewport* viewport, float width, float height );
+
 private Q_SLOTS:
   /** @brief Call updateProperties() on all SelectionHandlers in the
    * current selection. */
@@ -284,6 +287,14 @@ private:
 
   typedef std::map<std::string, ros::Publisher> PublisherMap;
   PublisherMap debug_publishers_;
+
+  struct M_OrthoConfig
+  {
+    float width, height;
+    Ogre::Viewport* viewport;
+  };
+  std::vector<M_OrthoConfig> ortho_config_;
+  QTimer* timer_;
 };
 
 } // namespace rviz
